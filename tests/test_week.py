dfbccc4fdf0404412635b640ce22ef8ec8c0fa11
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-11-21 15:58:00 +0900

    Fix autograding 2

--- tests/test_week.py
+++ tests/test_week.py
@@ -1,7 +1,14 @@
-from os import path
 from unittest import TestCase
 
 
 class TestWeek(TestCase):
-    def test_main_exists(self):
-        self.assertTrue(path.isfile('main.py'))
+    def test_week(self):
+        from package.subpackage.my_module2 import week
+
+        self.assertEqual(week(1), 'понедельник')
+        self.assertEqual(week(2), 'вторник')
+        self.assertEqual(week(3), 'среда')
+        self.assertEqual(week(4), 'четверг')
+        self.assertEqual(week(5), 'пятница')
+        self.assertEqual(week(6), 'суббота')
+        self.assertEqual(week(7), 'воскресенье')
